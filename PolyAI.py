bdb7b8e29141c61a7b46c00df532d6bd459913b1
Joshua Khalen <joshuakhalen@Joshuas-MacBook-Pro.local>
2022-05-26 18:28:43 +0100

    Updates

--- PolyAI.py
+++ PolyAI.py
@@ -9,14 +9,23 @@ for i in range(M):
 	points.append([X, Y, R])
 
 
-grid = np.zeros((N,N), dtype = int)
+grid = np.zeros((N, N), dtype = int)
+
 
 for x in range(len(points)):#
-	print(points[x])
-	i = 0
-	while i < points[x][2]:
-		grid[points[x][0]+i][points[x][1] + i] = 1 + grid[points[x][0]+i][points[x][1] + i] 
-		i= i +1
+	i = 1
+	while i < (points[x][2] + 1) :
+		if not points[x][0] + 1 < 0 and not points[x][1] + 1 <0:
+			grid[points[x][0]+i][points[x][1]] = 1 + grid[points[x][0]+i][points[x][1] + i]
+			grid[points[x][0]][points[x][1] + i] = 1 + grid[points[x][0]+i][points[x][1] + i]
+		if not points[x][0] - 1 < 0 and not points[x][1] -1 < 0:
+			grid[points[x][0] - i][points[x][1]]     = 1 + grid[points[x][0] - i][points[x][1]]
+			grid[points[x][0]][points[x][1] - i]     = 1 + grid[points[x][0]][points[x][1] - i]
+
+		#grid[points[x][0] - i][points[x][1]] = 1 + grid[points[x][0]+i][points[x][1] + i]
+		#grid[points[x][0]][points[x][1] - i] = 1 + grid[points[x][0]+i][points[x][1] + i]
+
+		i = i +1
 
 
 
