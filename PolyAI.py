7404b33197a43521a6f3d9d49f1c67f637621c09
Joshua Khalen <joshuakhalen@Joshuas-MacBook-Pro.local>
2022-05-27 18:55:08 +0100

    Final update

--- PolyAI.py
+++ PolyAI.py
@@ -1,65 +1,57 @@
 import numpy as np 
 
-N, M  = [int(x) for x in input('Debug; ').split()]
-points = []
-# https://stackoverflow.com/questions/58348401/numpy-array-filled-in-diamond-shape
-for i in range(M):
-	X, Y, R = [int(x) for x in input('Loc ').split()] 
-	points.append([X, Y, R])
+locations = [] # Storing Pizzaria Locations and Delivery Range
+grids = [] # Collating Pizzaria Maps
 
-#https://stackoverflow.com/questions/7115437/how-to-embed-a-small-numpy-array-into-a-predefined-block-of-a-large-numpy-arra
+with open('input.txt', 'r') as file:
+     lines = ([line.strip() for line in file])
+
+for i, x in enumerate(lines):
+	N, M = int(lines[0][0]), int(lines[0][2])
+	if i > 0:
+		X, Y, R = int(lines[i][0]), int(lines[i][2]), int(lines[i][4])
+		locations.append([X, Y, R])
+
+# Debug
+if len(locations) > M:
+	print("Dimensional error: number of points give is greater greater than N")
+
+
+#N, M = [int(x) for x in input().split()]  # Obtaining Grid Dims and Number of Pizzarias
+#for i in range(M): # Obtaining locations and ranges of Pizzarias
+#	X, Y, R = [int(x) for x in input().split()]
+#	locations.append([X, Y, R])
+
 def paste_slices(tup):
   pos, w, max_w = tup
-  wall_min = max(pos, 0)
-  wall_max = min(pos+w, max_w)
-  block_min = -min(pos, 0)
-  block_max = max_w-max(pos+w, max_w)
-  block_max = block_max if block_max != 0 else None
-  return slice(wall_min, wall_max), slice(block_min, block_max)
+  minGrid, maxGrid = max(pos, 0), min(pos+w, max_w)
+  minArray, maxArray = -min(pos, 0),  max_w-max(pos+w, max_w)
+  maxArray = maxArray if maxArray != 0 else None
+  return slice(minGrid, maxGrid), slice(minArray, maxArray)
 
-def paste(wall, block, loc):
-  loc_zip = zip(loc, block.shape, wall.shape)
-  wall_slices, block_slices = zip(*map(paste_slices, loc_zip))
-  wall[wall_slices] = block[block_slices]
+def paste(grid, array, loc):
+  loc_zip = zip(loc, array.shape, grid.shape)
+  grid_slices, array_slices = zip(*map(paste_slices, loc_zip))
+  grid[grid_slices] = array[array_slices]
 
-grids = []
 
-for x in range(len(points)):
+for x in range(len(locations)):
 	grid = np.zeros((N, N), dtype=int)
-	r = points[x][2] -1
+	r = locations[x][2]
 
 	array = np.add.outer(*[np.r_[:r, r:-1:-1]] * 2) >= r
 	array = array * 1
 
 	disCentre = array.shape[0]//2 + 1
+	paste(grid, array, (locations[x][0] - disCentre ,locations[x][1]- disCentre))
 
-	paste(grid,array,(points[x][0] - disCentre ,points[x][1]- disCentre))
-	print(grid)
-	print('--------------------------------')
+	grid = np.rot90(grid)
+
 	grids.append(grid)
-#for x in range(len(points)):#
-	#i = 1
-	#grid[points[x][0]-1][points[x][1]-1] = 1 + grid[points[x][0]-1][points[x][1]-1]
 
 print(grids)
 print(sum(grids))
 print(np.max(sum(grids)))
-
-	#while i < (points[x][2] + 1) :
-		#if not points[x][0] + i -1  < 0 and not points[x][1] + i -1 < 0 and points[x][1] + i - 1 <= len(grid) - 1 and points[x][1] -1 + i <= len(grid) -1 :
-		#	grid[points[x][0]+i-1][points[x][1]-1] = 1 + grid[points[x][0]+i-1][points[x][1]-1]
-			#grid[points[x][0]-1][points[x][1] + i -1] = 1 + grid[points[x][0]-1][points[x][1] + i - 1]
-
-
-
-		#if not points[x][0] - i - 1 < 0 and not points[x][1] - i - 1 < 0 and points[x][1] - i - 1 <= len(grid) - 1 and points[x][1] - 1- i <= len(grid) -1 :
-		#	grid[points[x][0] - i - 1][points[x][1] - 1]     = 1 + grid[points[x][0] - i -1][points[x][1] - 1]
-			#grid[points[x][0] - 1][points[x][1] - i - 1]     = 1 + grid[points[x][0] - 1][points[x][1] - i -1]
-
-		#grid[points[x][0] - i][points[x][1]] = 1 + grid[points[x][0]+i][points[x][1] + i]
-		#grid[points[x][0]][points[x][1] - i] = 1 + grid[points[x][0]+i][points[x][1] + i]
-
-		#i = i +1
 
 
 
