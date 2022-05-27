0ea0b9d1dd5a7e5b1a06dfad0fba4f7deb359d08
JoshT-H <joshua.tampoeholmes@gmail.com>
2022-05-27 20:12:07 +0100

    Final

--- PolyAI.py
+++ PolyAI.py
@@ -1,8 +1,15 @@
-import numpy as np 
+import numpy as np
+locations = [] # Storing pizzeria locations and delivery range
+grids = [] # Collating individual pizzeria maps
 
-locations = [] # Storing Pizzaria Locations and Delivery Range
-grids = [] # Collating Pizzaria Maps
 
+# Initial parsing method for variable via user inputs
+#N, M = [int(x) for x in input().split()]  # Obtaining Grid Dims and Number of Pizzarias
+#for i in range(M): # Obtaining locations and ranges of Pizzarias
+#	X, Y, R = [int(x) for x in input().split()]
+#	locations.append([X, Y, R])
+
+# Second parsing method of text file as per requirement
 with open('input.txt', 'r') as file:
      lines = ([line.strip() for line in file])
 
@@ -12,45 +19,62 @@ for i, x in enumerate(lines):
 		X, Y, R = int(lines[i][0]), int(lines[i][2]), int(lines[i][4])
 		locations.append([X, Y, R])
 
-# Debug
-if len(locations) > M:
-	print("Dimensional error: number of points give is greater greater than N")
+## Debug if the message for mismatch between N and the number of Pizzeria locations given.
+#if len(locations) > M:
+	#print("Dimensional error: number of points give is greater greater than N")
 
 
-#N, M = [int(x) for x in input().split()]  # Obtaining Grid Dims and Number of Pizzarias
-#for i in range(M): # Obtaining locations and ranges of Pizzarias
-#	X, Y, R = [int(x) for x in input().split()]
-#	locations.append([X, Y, R])
+def sliceArray(tup):
+	"""
+	Ensures that elements of the mask that exist
+	outside the grid are not included, thus allowing
+	the mask to be placed onto the grid even if it falls off the edge.
+	"""
+	position, w, wMax = tup
+	minGrid, maxGrid = max(position, 0), min(position+w, wMax)
+	minArray, maxArray = -min(position, 0),  wMax-max(position+w, wMax)
+	maxArray = maxArray if maxArray != 0 else None
+	return slice(minGrid, maxGrid), slice(minArray, maxArray)
 
-def paste_slices(tup):
-  pos, w, max_w = tup
-  minGrid, maxGrid = max(pos, 0), min(pos+w, max_w)
-  minArray, maxArray = -min(pos, 0),  max_w-max(pos+w, max_w)
-  maxArray = maxArray if maxArray != 0 else None
-  return slice(minGrid, maxGrid), slice(minArray, maxArray)
+def insertArray(grid, array, loc):
+	"""
+	Inserts smaller pizzeria array into larger world array - similar to placing a mask on an image.
+	"""
+	loc_zip = zip(loc, array.shape, grid.shape)
+	grid_slices, array_slices = zip(*map(sliceArray, loc_zip))
+	grid[grid_slices] = array[array_slices]
 
-def paste(grid, array, loc):
-  loc_zip = zip(loc, array.shape, grid.shape)
-  grid_slices, array_slices = zip(*map(paste_slices, loc_zip))
-  grid[grid_slices] = array[array_slices]
-
+def pizzeriaArray(r):
+	"""
+	Take the delivery range of each pizzeria as argument
+	 and generates an array highlighting the location of said
+	 pizzeria and the tiles it will deliver to.
+	"""
+	# Generates a boolean array; True = will deliver and False = will not deliver
+	smallArray = np.add.outer(*[np.r_[:r, r:-1:-1]] * 2) >= r
+	return smallArray * 1 # Convert bool-array => int-array
 
 for x in range(len(locations)):
 	grid = np.zeros((N, N), dtype=int)
-	r = locations[x][2]
+	smallArray = pizzeriaArray(locations[x][2]) # Argument is delivery radius
+	disCentre = smallArray.shape[0] // 2 + 1 # Obtain distance to center i.e location of pizzeria
 
-	array = np.add.outer(*[np.r_[:r, r:-1:-1]] * 2) >= r
-	array = array * 1
+	# Inserts smaller pizzeria array onto larger grid/world array
+	insertArray(grid, smallArray, (locations[x][0] - disCentre , locations[x][1] - disCentre))
 
-	disCentre = array.shape[0]//2 + 1
-	paste(grid, array, (locations[x][0] - disCentre ,locations[x][1]- disCentre))
-
+	# The (0,0) coordinate is as the top of the grid thus it must be rotated.
 	grid = np.rot90(grid)
 
+	# Individually generating grids with the pizzerias projected onto them.
 	grids.append(grid)
 
-print(grids)
-print(sum(grids))
+# Debugging
+# Printing a list containing all the individual grids
+#print(grids)
+# Printing the resultant grid map
+#print(sum(grids))
+
+# Summing all the grids in the list to generate a map which includes all the pizzerias and finding the maximum value in the array.
 print(np.max(sum(grids)))
 
 
